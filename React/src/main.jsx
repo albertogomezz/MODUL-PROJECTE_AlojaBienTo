5cd2b6075515a4864dd4ae7d12ff7b26a17f89f7
Alberto <albertogom.alon@gmail.com>
2024-03-24 22:27:18 +0100

    Changes

--- React/src/main.jsx
+++ React/src/main.jsx
@@ -5,11 +5,9 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
-import { createRoot } from 'react-dom/client';
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import { i18nConf } from './i18n/config.js'
-import Swal from 'sweetalert2'
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
